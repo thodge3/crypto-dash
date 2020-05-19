347c083990af239c41835c93c8ce8242467c56a9
thodge3 <contact@travishodge.com>
2020-05-19 03:58:29 -0400

    Added Loading Content.

--- src/App/index.js
+++ src/App/index.js
@@ -6,13 +6,16 @@ import AppLayout from './AppLayout';
 import AppBar from './AppBar';
 import { AppProvider } from './AppProvider';
 import Settings from '../Settings';
+import Content from '../Shared/Content';
 
 function App() {
   return (
     <AppLayout>
       <AppProvider>
         <AppBar />
-        <Settings /> 
+        <Content>
+          <Settings /> 
+        </Content>
       </AppProvider>
     </AppLayout>
   );
