26db93f42bcab2a522cca3c111ce08aa2457ad4d
thodge3 <contact@travishodge.com>
2020-05-19 03:10:49 -0400

    Added AppProvider / Consumer to AppBar.

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { AppContext } from './AppProvider';
 
 const Logo = styled.div`
     font-size: 1.5em;
@@ -21,11 +22,17 @@ function toProperCase(lower){
     return lower.charAt(0).toUpperCase() + lower.substr(1);
 }
 
-function ControlButton({ name, active }){
+function ControlButton({ name }){
     return (
-    <ControlButtomElem active={ active }>
-        { toProperCase(name) }
-    </ControlButtomElem>
+    <AppContext.Consumer>
+        {({ page, setPage }) => (
+        <ControlButtomElem 
+            active={ page === name }
+            onClick={ () => setPage(name) }
+        >
+            { toProperCase(name) }
+        </ControlButtomElem>)}
+    </AppContext.Consumer>
     )
 }
 
@@ -34,7 +41,7 @@ export default function (){
     <Bar>
         <div> CryptoDash </div>
         <div/>
-        <ControlButton active name='Dashbord'/>
+        <ControlButton active name='Dashboard'/>
         <ControlButton name='Settings'/>
     </Bar>
     )
--- src/App/index.js
+++ src/App/index.js
@@ -5,12 +5,15 @@ import styled, { css } from 'styled-components';
 import WelcomeMessage from './WelcomeMessage';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
+import { AppProvider } from './AppProvider';
 
 function App() {
   return (
     <AppLayout>
-      <AppBar />
-      <WelcomeMessage /> 
+      <AppProvider>
+        <AppBar />
+        <WelcomeMessage /> 
+      </AppProvider>
     </AppLayout>
   );
 }
