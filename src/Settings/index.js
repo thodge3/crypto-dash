2eb863bdc95be3a48e48329da7e94cfef775eb40
thodge3 <contact@travishodge.com>
2020-05-19 03:53:54 -0400

    Added Page component.

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
+import Page from '../Shared/Page';
 
 export default function(){
     return (
-        <div>
+        <Page name='Settings'>
             <WelcomeMessage />
             <ConfirmButton />
-        </div>
+        </Page>
     ) 
 }
